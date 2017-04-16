bff806650b8a81be21c9a0bc4afecdcd24cdc49a
veit <veit@blueshoe.de>
2017-04-16 20:59:00 +0200

    setup.py changes

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
+from accessor import __version__
+
 setup(
     name='accessor',
-    version='0.1',
+    version=__version__,
     description='A simple python module for resolving paths on python objects',
     long_description=open('README.rst').read(),
     author='Veit Rückert',
     author_email='veit.rueckert@web.de',
     maintainer='Veit Rückert',
     maintainer_email='veit.rueckert@web.de',
-    url='http://github.com/schwobaseggl/accessor',
+    url='http://github.com/schwobaseggl/accessor/archive/v{}.zip'.format(__version__),
     packages=find_packages(where=('accessor',)),
     keywords="accessor attribute key index resolve",
     classifiers=[
